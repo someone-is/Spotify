dfa1df4289e2156b799e6b83be0c720694629303
someone-is <shouryashekhar.31december@gmail.com>
2022-10-31 12:31:38 +0530

    new update

--- components/SearchAlbum.js
+++ components/SearchAlbum.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
 import { urlFor } from '../library/client'
 
 function SearchAlbum({songs}) {
+  const [first, setfirst] = useState(false)
+  const [width, setwidth] = useState()
+  const detectSize = () => {
+    setwidth(window.innerWidth)
+}
+  useEffect(() => {
+    window.addEventListener('resize', detectSize)
+    if (window.innerWidth <= 600) {
+        setfirst(true)
+    }
+    else {
+        setfirst(false)
+    }
+    return () => {
+        window.removeEventListener('resize', detectSize)
+    }
+}, [width])
+
   return (
     <>
-    {/* <div className='topone'>{songs.name}</div> */}
-    <div className="tile">
+    {first?( <div className="searchplate">
+    <div className="searchsmallplaypauseno">
+        {/* {songs.track.id === activeSong && isPlaying ? (
+            <Image src="/image/SmallPause.png" width={20} height={20} onClick={Play} alt="play" />
+        ) : (
+            <Image src="/image/SmallPlay.png" width={20} height={20} onClick={Play} alt="pause" />
+        )} */}
+        {/* <span>{tracker+1}</span> */}
+        <div className="searching">
+        <Image className='searchplaypause' src="/image/SmallPlay.png" width={20} height={20} alt="pause" />
+        <img className='searchcover' src={urlFor(songs.cover)} alt={songs.name} width={45} height={45}/>
+        </div>
+    </div>
+    {/* <div className="thumb"> */}
+        {/* <Image src={songs.cover.id} width={50} height={50}/> */}
+    {/* </div> */}
+    <div className="searchSongandartists">
+        <h3>{songs.name}</h3>
+        <p>{songs.artists}</p>
+    </div>
+    {/* <p className="DURATION">{songs.duration}</p> */}
+
+</div>):(<div className="tile">
         <img className="cover" src={urlFor(songs.cover)} alt="" />
         <img className='GreenPlay' src="../image/GreenPlay.png" alt="pause"/>
         {/* <Link href={"/"+`${tracker}=`+songs.slug.current} key={songs.slug.current}> */}
@@ -14,6 +54,8 @@ function SearchAlbum({songs}) {
         <p>{songs.artists}</p>
         
       </div>
+    )}
+    {/* <div className='topone'>{songs.name}</div> */}
     </>
   )
 }
