c3cf129790f07a8d92cfc2f4cacd053710c0cefc
someone-is <shouryashekhar.31december@gmail.com>
2022-11-06 22:31:35 +0530

    light mode

--- pages/[Album].js
+++ pages/[Album].js
@@ -8,15 +8,16 @@ function Album({ song, colours, music }) {
   const { Album } = router.query;
 
   const album = Album.split("=");
-  
+  console.log(colours)
+
   return (
     <>
-      <div className="container" style={{ background: `linear-gradient(to bottom, ${colours.cover.id.colour.dominant.background}, #000000 45%)` }}>
- 
-          <div onClick={() => router.back()} >
-  
-            <img className='navigationbutton' src="../image/back.png" alt="" width="35" />
-          </div>
+      <div className="container">
+
+        <div onClick={() => router.back()} >
+
+          <img className='navigationbutton' src="../image/back.png" alt="" width="35" />
+        </div>
 
         <img className='Albumbigcover' src={urlFor(song.cover)} alt={song.name} />
         <div className="det">
@@ -28,6 +29,23 @@ function Album({ song, colours, music }) {
           </div>
         </div>
       </div>
+      <style jsx>{`
+      
+       .container{
+      background: linear-gradient(to bottom, ${colours.cover.id.colour.dominant.background}, #000000 45%);
+       }
+      @media (prefers-color-scheme: light) {
+  html {
+    color-scheme: light;
+  }
+  .dettt {
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0)2%, white 30%);
+  }
+  .container{
+    background:linear-gradient(to bottom, ${colours.cover.id.colour.lightVibrant.background}, ${colours.cover.id.colour.lightVibrant.background}00 45%)
+  }
+}
+  `}</style>
     </>
   )
 }
@@ -40,7 +58,7 @@ export const getServerSideProps = async (context) => {
   const songs = await client.fetch(query);
   const cover = '*[_type == "songs"]{cover {"id":asset->.url}}';
   const covers = await client.fetch(cover);
-  const color = `*[_type == "songs" && slug.current=="${album[1]}"][0]{cover {"id":asset->.metadata{"colour":palette{dominant{background}}}}}`;
+  const color = `*[_type == "songs" && slug.current=="${album[1]}"][0]{cover {"id":asset->.metadata{"colour":palette}}}`;
   const colours = await client.fetch(color);
   const pagesongs = `*[_type == "songs" && slug.current=="${album[1]}"][0]{_id,artists,album,cover{ "id":asset->.url},song[]->{_id,name,artists,album,duration,slug{current},track{"id":asset->.url}}}`;
   const music = await client.fetch(pagesongs);
